38db6d43ec6d8796c38154be10429983229a58c6
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-12-03 11:36:17 +0500

    created products admin panel

--- store/products/models.py
+++ store/products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -13,3 +16,6 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Продукт: {self.name} | Категория: {self.category.name}"
