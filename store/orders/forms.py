1829a9a702510fdd04bcd037f2e9a1044c081529
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-12-15 11:51:22 +0500

    created order view, urls and forms

---
+++ store/orders/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from orders.models import Order
+
+
+class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'})
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'you@example.com'})
+    )
+    address = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Россия, Москва, ул. Мира, дом 6'})
+    )
+    class Meta:
+        model = Order
+        fields = ('first_name', 'last_name', 'email', 'address')
--- store/orders/views.py
+++ store/orders/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from orders.forms import OrderForm
+from django.urls import reverse_lazy
+from common.views import TitleMixin
 
-# Create your views here.
+
+class OrderCreateView(TitleMixin, CreateView):
+    template_name = 'orders/order-create.html'
+    form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
+    title = 'Store - Оформление заказа'
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super().form_valid(form)
